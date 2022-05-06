eb19881bce2587728761c5589eb1462a1c4a432d
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-05-06 22:15:05 +0200

    Update customize-image.sh

--- images/openhab/customize-image.sh
+++ images/openhab/customize-image.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-echo "Installing OpenHab"
+echo "Installing Openhab Docker instance"
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt install docker-ce
+
+sudo groupadd -g 9001 openhab
+sudo useradd -g 9001 openhab
+sudo usermod -aG docker openhab
+
+su openhab
+docker run \
+        --name openhab \
+        --net=host \
+        -v /etc/localtime:/etc/localtime:ro \
+        -v /etc/timezone:/etc/timezone:ro \
+        -v openhab_addons:/openhab/addons \
+        -v openhab_conf:/openhab/conf \
+        -v openhab_userdata:/openhab/userdata \
+        -d \
+        --restart=always \
+        openhab/openhab:3.2.0
