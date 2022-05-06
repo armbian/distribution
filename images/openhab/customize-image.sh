20511706c9f9fa7627e1fc9b6c8376d1958ac011
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-05-06 22:32:20 +0200

    Update customize-image.sh

--- images/openhab/customize-image.sh
+++ images/openhab/customize-image.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-echo "Installing Openhab Docker instance"
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+echo "Read / only rootfs"
+sudo apt-get -y install overlayroot
+sudo sed -i "s/^overlayroot=.*/overlayroot=\"tmpfs\"/" /etc/overlayroot.conf
+sudo sed -i "s/^overlayroot_cfgdisk=.*/overlayroot_cfgdisk=\"enabled\"/" /etc/overlayroot.conf
+echo "Installing Openhab Docker instance"
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt install docker-ce
-
+sudo apt-get -y install docker-ce
 sudo groupadd -g 9001 openhab
 sudo useradd -g 9001 openhab
 sudo usermod -aG docker openhab
-
 su openhab
 docker run \
         --name openhab \
