3eac6bd2b370bc112134d6f26664db52e2bb1fb5
Igor Pečovnik <igorpecovnik@users.noreply.github.com>
2022-05-10 21:12:37 +0200

    Update customize-image.sh

--- images/openhab/customize-image.sh
+++ images/openhab/customize-image.sh
@@ -9,15 +9,15 @@ groupadd -g 9001 openhab
 useradd -g 9001 openhab
 usermod -aG docker openhab
 su openhab
-#docker run \
-#       --privileged \
-#       --name openhab \
-#       --net=host \
-#       -v /etc/localtime:/etc/localtime:ro \
-#       -v /etc/timezone:/etc/timezone:ro \
-#       -v openhab_addons:/openhab/addons \
-#       -v openhab_conf:/openhab/conf \
-#       -v openhab_userdata:/openhab/userdata \
-#       -d \
-#       --restart=always \
-#       openhab/openhab:3.2.0
+docker run \
+       --privileged \
+       --name openhab \
+       --net=host \
+       -v /etc/localtime:/etc/localtime:ro \
+       -v /etc/timezone:/etc/timezone:ro \
+       -v openhab_addons:/openhab/addons \
+       -v openhab_conf:/openhab/conf \
+       -v openhab_userdata:/openhab/userdata \
+       -d \
+       --restart=always \
+       openhab/openhab:3.2.0
